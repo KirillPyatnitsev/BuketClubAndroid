82567f0bbc14456037011c03ba95c1193ae9c75b
mifkamaz <mif.kam.az.reg@gmail.com>
2015-12-08 09:39:14 +0300

    +BouquetDetalis Activity

---
+++ app/src/main/java/ru/creators/buket/club/model/Bouquet.java
@@ -0,0 +1,7 @@
+package ru.creators.buket.club.model;
+
+/**
+ * Created by mifkamaz on 07/12/15.
+ */
+public class Bouquet {
+}
---
+++ app/src/main/java/ru/creators/buket/club/view/activitys/BucketDetalisActivity.java
@@ -0,0 +1,13 @@
+package ru.creators.buket.club.view.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BucketDetalisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bucket_detalis);
+    }
+}
